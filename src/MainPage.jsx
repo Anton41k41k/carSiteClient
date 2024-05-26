af3c1c7ceb67a00acf93bcb3490749032c6cbfa8
Anton <you@example.com>
2024-05-27 02:34:18 +0300

    add lazy loading component with content

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,21 +1,22 @@
 import { Box, CircularProgress } from "@mui/material";
 import "./mainPage.css";
 import MainImgSection from "./components/mainComponents/MainImgSection";
-import { lazy, useState } from "react";
+import { Suspense, lazy, useState } from "react";
 const MainPageInformaton = lazy(() =>
   import("./components/mainComponents/MainPageInformation")
 );
 
 export default function MainPage() {
-  const [imageLoaded, setImageLoaded] = useState();
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
     <Box component={"main"} className="main">
       <MainImgSection setImageLoaded={setImageLoaded} />
-      {imageLoaded ? (
-        <MainPageInformaton />
-      ) : (
-        <CircularProgress sx={{ mt: "15%" }} />
+      {!imageLoaded && <CircularProgress sx={{ margin: "15% 50vw" }} />}
+      {imageLoaded && (
+        <Suspense>
+          <MainPageInformaton />
+        </Suspense>
       )}
     </Box>
   );
