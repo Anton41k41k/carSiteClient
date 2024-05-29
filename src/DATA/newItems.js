14e8b4148b5cdaf5e6d8c0d9a73a8e82e6b96451
Anton <you@example.com>
2024-05-29 04:03:21 +0300

    delete one element

--- src/DATA/newItems.js
+++ src/DATA/newItems.js
@@ -6,33 +6,27 @@ export const newItems = [
     id: "1",
   },
   {
-    title: "Восторг электромобильности",
-    text: "Совершенно новое, заряженное эмоциями удовольствие от вождения. Плавное ускорение при трогании с места. Электризующая динамика в сочетании с комфортом и спокойствием – практически бесшумное движение.",
-    img: "https://www.bmw.ru/content/dam/bmw/common/topics/fascination-bmw/electromobility2020-new/bev-new/bmw-emobility-bev-ms-electric-car-lifestyle.jpg/jcr:content/renditions/cq5dam.resized.img.585.low.jpg",
-    id: "2",
-  },
-  {
     title: "Электромобильность: сборка на новом уровне",
     text: "Устойчивое и гибкое: Audi модернизирует, оцифровывает и преобразует свои заводы для серии Q6 e-tron, включая свою штаб-квартиру в Ингольштадте.",
     img: "https://www.audi.com/content/dam/gbp2/innovation/product-innovation/drive-systems/e-mobility/production-audi-q6-e-tron/1920x1080_A241181_large.jpg?imwidth=477&imdensity=1",
-    id: "3",
+    id: "2",
   },
   {
     title: "Совершенно новый электрический G-класс",
     text: "Испытайте плавную интеграцию мощности и производительности, которая сделает ваши поездки такими же легкими, как и электрифицирующими. Благодаря оптимизированному управлению аккумулятором и оптимизированным энергетическим системам полностью электрический G-Класс всегда готов к будущему.",
     img: "https://www.mercedes-benz.de/content/germany/de/passengercars/models/suv/n465-24-1/overview/_jcr_content/root/responsivegrid/media_slider/media_slider_item/image.component.damq6.3426579305725.jpg/mercedes-benz-g-class-n465-highlights-exterior-856x480-03-2024.jpg",
-    id: "4",
+    id: "3",
   },
   {
     title: "Smart Geely 2025",
     text: "Стратегия Smart Geely 2025 в качестве ключевых элементов предусматривает электрификацию модельного ряда, а также обеспечение устойчивого развития сервисов мобильности. Электромобили и автомобили с гибридными силовыми установками разных конфигураций - HEV и PHEV с увеличенным запасом хода на электрической тяге - станут основой линейки брендов группы Geely Auto.",
     img: "https://www.geely-motors.com/local/templates/geely_main/frontend_bitrix/build/img/partner_car.jpg",
-    id: "5",
+    id: "4",
   },
   {
     title: "Электромобиль BMW идеально дополнит ваш стиль жизни",
     text: "Удовольствие за рулем без выброса вредных веществ. Оптимальная практичность в повседневной эксплуатации благодаря высокому запасу хода. Благодаря непрерывному развитию зарядной инфраструктуры продолжительные поездки становятся все более удобными",
     img: "https://www.bmw.ru/content/dam/bmw/common/topics/fascination-bmw/electromobility2020-new/bev-new/bmw-emobility-bev-ms-electric-car-lifestyle.jpg/jcr:content/renditions/cq5dam.resized.img.585.low.time1653343472521.jpg",
-    id: "6",
+    id: "5",
   },
 ];
